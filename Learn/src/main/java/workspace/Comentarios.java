7e6832a679bf61b6bce338789dbf4fdee65e3cf6
CalixtoTech <103325008+ClxtMf@users.noreply.github.com>
2024-11-25 10:15:06 -0300

    Update

---
+++ Learn/src/main/java/workspace/Comentarios.java
@@ -0,0 +1,13 @@
+/*
+Comentário aqui
+podemos ter quantas linhas quisermos.
+ */
+
+
+ /**
+  * Comentário melhor formato.
+  * Podemos ter quantas linhas quisermos.
+ **/
+
+ // Comentário aqui.
+ // Para ter outra linha, preciso colocar duas barras novamente.
---
+++ Learn/src/main/java/workspace/Hello.java
@@ -0,0 +1,7 @@
+package WorkSpace;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.err.println("Hello World!!!");
+    }
+}
